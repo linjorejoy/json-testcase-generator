044ec33a8787d82bb787a979818a417e96cc2ab1
Linjo Rejoy <howyoudoing1618@gmail.com>
2021-04-12 12:48:54 +0530

    start organising

---
+++ src/helperobjects/OutputFiles.py
@@ -0,0 +1,17 @@
+from helperobjects.OutputJsonFile import OutputJsonFile
+
+from typing import List
+
+class OutputFiles:
+
+    def __init__(self):
+        self.count = 0
+        self.output_json_file_array:List[OutputJsonFile] = []
+
+    def add_output_json_file(self, json_file_obj:OutputJsonFile):
+        self.count += 1
+        self.output_json_file_array.append(json_file_obj)
+
+    def get_output_json_file_array(self):
+        return self.output_json_file_array
+        
